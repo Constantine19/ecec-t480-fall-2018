1849d710ff3c89f5d0558a4358acd5127c2d793a
Konstantin Zelmanovich <konstantin@siftsec.com>
2018-11-07 21:52:07 -0500

    Edit cf_polyfit

--- HW5/cf_polyfit.m
+++ HW5/cf_polyfit.m
@@ -1,12 +1,25 @@
-function [a, r_2, syx] = cf_polyfit(x,y)
+function [A, r2] = cf_polyfit2(coordinate, order)
 
-x = x';
-y = y';
-z = [ones(size(x)) x x.^2];
-a = z\y;
+vals = zeros(order+1, length(coordinate))';
+vals(1:end,1) = 1;
+xvals = coordinate(1:end,1);
+yvals = coordinate(1:end,2);
 
-Sr = sum((y-z*a).^2);
-r_2 = 1 - Sr / sum((y - mean(y)).^2);
-syx = sqrt(Sr/(length(x)-length(a)));
-   
+St = 0;
+Sr = 0;
+n = length(xvals);
+a1 = (n*sum(xvals .* yvals) - sum(xvals) * sum(yvals)) / (n*sum(xvals.^2) - (sum(xvals))^2);
+a0 = (sum(yvals)/length(yvals)) - a1*(sum(xvals)/n);
+vals(1:end,2) = xvals;
+for i = 1:length(coordinate)
+    for j = 2:order
+        vals(i,j+1) = xvals(i)^(j);
+    end
+    St = St + (yvals(i) - (sum(yvals)/length(yvals)))^2;
+    Sr = Sr + (yvals(i) - a0 - a1*xvals(i))^2;
+end
+
+A = inv(vals' * vals)*vals'*yvals;
+A = flip(A);
+r2 = (St - Sr)/St;
 end
--- HW5/run.m
+++ HW5/run.m
@@ -1,8 +1,18 @@
 
+
 %% Problem 3
 clc
 clear all
 
 x = [0, 5, 10, 15, 20];
 y = [100, 200, 450, 950, 2000];
-[a, r2, syx] = cf_polyfit(x,y)
+z = [x;y]';
+[a, r2] = cf_polyfit(z,3)
+
+%% Problem 4
+clc
+clear all
+x = [ 0, 2, 4, 6, 9, 11, 12, 15, 17, 19];
+y = [5, 6, 7, 6, 9, 8, 7, 10, 12, 12]; 
+z = [x;y]';
+[a, r2] = cf_polyfit(z,5)
