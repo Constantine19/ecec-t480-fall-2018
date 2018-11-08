4a2ac2aa6c50c8980a4e1baf6b2d997fcdb6fde5
Constantine19 <czelmanovich@gmail.com>
2018-11-07 20:27:14 -0500

    Add cf_polyfit and Problem3

--- HW5/cf_polyfit.m
+++ HW5/cf_polyfit.m
@@ -1,1 +1,12 @@
-function [coeff, r_2] = cf_polyfit(x, y, order, stdev)
+function [a, r_2, syx] = cf_polyfit(x,y)
+
+x = x';
+y = y';
+z = [ones(size(x)) x x.^2];
+a = z\y;
+
+Sr = sum((y-z*a).^2);
+r_2 = 1 - Sr / sum((y - mean(y)).^2);
+syx = sqrt(Sr/(length(x)-length(a)));
+   
+end
---
+++ HW5/run.m
@@ -0,0 +1,8 @@
+
+%% Problem 3
+clc
+clear all
+
+x = [0, 5, 10, 15, 20];
+y = [100, 200, 450, 950, 2000];
+[a, r2, syx] = cf_polyfit(x,y)
